041320149f6049ef7166c2ace32c68d24036a37e
Tsunemichi Harada <fox10225fox@gmail.com>
2018-03-27 00:40:57 +0900

    change .gitignore & init.el

---
+++ init.el
@@ -0,0 +1,10 @@
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(fset 'package-desc-vers 'package--ac-desc-version)
+(package-initialize)
+
